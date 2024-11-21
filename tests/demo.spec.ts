63cfd2baab1a8bd04533823212d3a8c69e40c4da
0xEthan <sdzdj01@163.com>
2024-11-21 23:35:02 +0800

    init: ethan-demo-sdk

---
+++ tests/demo.spec.ts
@@ -0,0 +1,18 @@
+import { assert } from "chai";
+import { A, EthersWallet, B } from "../src";
+
+describe("demo", () => {
+  it("should A", async () => {
+    assert.equal(A, "A");
+  });
+
+  it("should B", async () => {
+    assert.equal(B, "B");
+  });
+
+  it("should createRandomWallet", async () => {
+    const wallet = EthersWallet.createRandom();
+    console.log(wallet.address);
+    assert.equal(wallet.address.length, 42);
+  });
+});
