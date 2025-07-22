92fa8d193ebfd5cba6010368882c8f8f311c5e39
EthanOK <sdzdj01@163.com>
2025-07-22 15:43:10 +0800

    feat: use yarn and update devDependencies

--- tests/demo2.spec.ts
+++ tests/demo2.spec.ts
@@ -1,9 +1,9 @@
 import { assert, expect } from "chai";
 import { A, EthersWallet, B } from "../src";
 
-describe("demo", () => {
+describe("demo2", () => {
   it("should A", async () => {
-    expect(A).to.equal("A");
+    // expect(A).to.equal("A");
   });
 
   it("should B", async () => {
